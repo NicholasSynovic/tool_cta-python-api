449d55e0099844dfe5f0e728390fe559c585b46e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-12-06 20:55:45 -0600

    Add methods to handle parameter types

--- cta/_builder/__init__.py
+++ cta/_builder/__init__.py
@@ -1,6 +1,6 @@
 import logging
-import typing
-import urllib.parse
+from typing import Any, List, Tuple
+from urllib.parse import urlencode
 
 
 def _constructAPI(url: str, **kwargs) -> str:
@@ -14,10 +14,10 @@ def _constructAPI(url: str, **kwargs) -> str:
     :return: A formatted URL with non-null parameters
     :rtype: str
     """  # noqa: E501
-    data: dict[str, typing.Any] = {}
+    data: dict[str, Any] = {}
 
     key: str
-    val: typing.Any
+    val: Any
     for key, val in kwargs.items():
         if val is None:
             continue
@@ -27,18 +27,18 @@ def _constructAPI(url: str, **kwargs) -> str:
 
         data[key] = val
 
-    params: str = urllib.parse.urlencode(query=data)
+    params: str = urlencode(query=data)
     return f"{url}?{params}"
 
 
-def _safeJoin(data: typing.Any, sep: str = ",") -> str | None:
+def _safeJoin(data: Any, sep: str = ",") -> str | None:
     """
     Given some data, try to create a string of the data where each value is seperated by a specific char or string
 
     String data is not modified. List data is joined by the seperator. All other datatypes are returned as None.
 
     :param data: Data to join together
-    :type data: typing.Any
+    :type data: Any
     :param sep: Char or string to seperate data values by, defaults to ","
     :type sep: str, optional
     :return: A string with each value seperated or None
@@ -55,3 +55,34 @@ def _safeJoin(data: typing.Any, sep: str = ",") -> str | None:
             raise TypeError
 
     return None
+
+
+def _checkNotNoneAndType(obj: Any, types: Any | Tuple[Any]) -> bool:
+    """
+    Return true if `obj` is not None and is of a valid type in `types`.
+
+    :param obj: The object to test
+    :type obj: Any
+    :param types: The type or types (as a tuple) to test the object against
+    :type types: Any | Tuple[Any]
+    :raises TypeError: If `types` is not a type or a Tuple of types, raise TypeError
+    :return: True if `obj` is not None and is of type `types`
+    :rtype: bool
+    """
+    if obj is None:
+        return False
+
+    try:
+        return isinstance(obj, types)
+    except TypeError:
+        raise TypeError("`types` must be a Tuple of types")
+
+
+def _checkInList(obj: Any, _list: List[Any]) -> None:
+    if _checkNotNoneAndType(obj=_list, types=(list, tuple)) is False:
+        raise AttributeError("`_type` is not of type List or Tuple")
+
+    try:
+        _list.index(obj)
+    except ValueError:
+        raise ValueError(f"`{obj}` not in `_list`: `{_list}`")
--- test/test_builder_init.py
+++ test/test_builder_init.py
@@ -2,21 +2,24 @@ from typing import List
 
 import pytest
 
-from cta._builder import _constructAPI, _safeJoin
+from cta._builder import (
+    _checkInList,
+    _checkNotNoneAndType,
+    _constructAPI,
+    _safeJoin,
+)
 
 
 def test_constructAPI() -> None:
     url: str = "https://test.com"
 
     assert _constructAPI(url=url, test="test") == "https://test.com?test=test"
-    assert _constructAPI(url=url, test=None) == "https://test.com?"
-    assert _constructAPI(url=url, test="") == "https://test.com?"
-    assert (
-        _constructAPI(url=url, test1=1, test2=2) == "https://test.com?test1=1&test2=2"
-    )
+    assert _constructAPI(url=url, test=None) == f"{url}?"
+    assert _constructAPI(url=url, test="") == f"{url}?"
+    assert _constructAPI(url=url, test1=1, test2=2) == f"{url}?test1=1&test2=2"
 
 
-def test_safeJoint() -> None:
+def test_safeJoin() -> None:
     string: str = "hello world"
     stringList: List[str] = ["hello", "world"]
     intList: List[int] = [1, 2]
@@ -26,3 +29,29 @@ def test_safeJoint() -> None:
 
     with pytest.raises(expected_exception=TypeError):
         _safeJoin(data=intList)
+
+
+def test_checkNotNoneAndType() -> None:
+    assert _checkNotNoneAndType(obj="Test", types=str) == True
+    assert _checkNotNoneAndType(obj="Test", types=(str, int)) == True
+
+    assert _checkNotNoneAndType(obj=None, types=str) == False
+    assert _checkNotNoneAndType(obj=1, types=str) == False
+    assert _checkNotNoneAndType(obj=1, types=(str, list)) == False
+
+    with pytest.raises(expected_exception=TypeError):
+        _checkNotNoneAndType(obj=1, types=[str, list])
+        _checkNotNoneAndType(obj=1, types=1)
+        _checkNotNoneAndType(obj=1, types="")
+
+
+def test_checkInList() -> None:
+    assert _checkInList(obj="test", _list=["test"]) is None
+    assert _checkInList(obj="test", _list=["test", "tree"]) is None
+
+    with pytest.raises(expected_exception=AttributeError):
+        assert _checkInList(obj="test", _list=1)
+        assert _checkInList(obj="test", _list="test")
+
+    with pytest.raises(ValueError):
+        _checkInList(obj=1, _list=["test"])
